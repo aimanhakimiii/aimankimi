cd264bb49d14ffcc3a91e56b29ef3a53cf49cc48
aimanhakimiii <159399509+aimanhakimiii@users.noreply.github.com>
2024-02-08 15:29:16 +0800

    Update advertising-sales-predict.py

--- advertising-sales-predict.py
+++ advertising-sales-predict.py
@@ -1,3 +1,4 @@
+%%writefile advertising-sales-predict.py
 import streamlit as st
 import pandas as pd
 import seaborn as sns
@@ -10,20 +11,21 @@ st.write("This app predicts the **Advertising Sales** type!")
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV = st.sidebar.slider('TV', 4.3, 7.9, 5.4)
-    Radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
-    Newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
+    TV = st.sidebar.slider('TV', 0.0, 300.0, 0.7)
+    Radio = st.sidebar.slider('Radio', 0.0, 50.0, 1.0)
+    Newspaper = st.sidebar.slider('Newspaper', 0.0, 200.0, 0.3)
     data = {'TV': TV,
             'Radio': Radio,
             'Newspaper': Newspaper,}
     features = pd.DataFrame(data, index=[0])
-    return features 
+    return features
 
 df = user_input_features()
 
 st.subheader('User Input parameters')
 st.write(df)
 modelAdvertising = pickle.load(open("modelAdvertising.h5,"rb))
+prediction = modelAdvertising.prefict(df)
 
 
 st.subheader('Prediction')
